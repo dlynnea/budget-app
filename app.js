01e851da3c9669427b50dda0d654970292cb5b5b
dlynnea <danimatton@gmail.com>
2020-05-22 13:05:38 -0700

    initial module architecture has been set up

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // budget controller
 var budgetController = (function() {
+  
     
 })();
 
@@ -13,6 +14,16 @@ var UIController = (function() {
 
 // app controller
 var controller = (function(budgetCtrl, UICtrl) {
-    
+
+    document.querySelector('.add-button').addEventListener('click', function() {
+        
+    });
+
+    document.addEventListener('keypress', function(event) {
+        if(event.keyCode === 13 || event.which === 13) {
+            
+        }
+    });
+
 
 })(budgetController, UIController);
