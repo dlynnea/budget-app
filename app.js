dfd105090be2c170df475b5bedc839120c7dc37c
dlynnea <danimatton@gmail.com>
2020-05-26 22:21:26 -0600

    making input types dynamic, dom string variables

--- app.js
+++ app.js
@@ -11,17 +11,21 @@ var UIController = (function() {
     var DOMstrings = {
         inputType: '.add-type',
         inputDescription: '.add-description',
-        inputValue: '.add-value'
+        inputValue: '.add-value',
+        inputBtn: '.add-button'
     }
 
     return {
         getInput: function() {
-
             return {
                 type: document.querySelector(DOMstrings.inputType).value,
                 description: document.querySelector(DOMstrings.inputDescription).value,
                 value: document.querySelector(DOMstrings.inputValue).value,
-            }
+            };
+        },
+
+        getDOMstrings: function () {
+            return DOMstrings;
         }
     };
 
@@ -31,12 +35,14 @@ var UIController = (function() {
 // app controller
 var controller = (function(budgetCtrl, UICtrl) {
 
+    var DOM = UICtrl.getDOMstrings();
+
     var addItem = function() {
         var input = UICtrl.getInput();
         console.log(input)
     }
 
-    document.querySelector('.add-button').addEventListener('click', addItem);
+    document.querySelector(DOM.inputBtn).addEventListener('click', addItem);
 
     document.addEventListener('keypress', function(event) {
         if(event.keyCode === 13 || event.which === 13) {
